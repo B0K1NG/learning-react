939ee754f31ec3fbba5ddfe274ca1fdbe054bc8c
B0K1NG <dominykaspavlijus@gmail.com>
2024-06-29 18:27:52 +0300

    Tweaks to ensure better readability.
    Imported useState and useReducer hooks;
    
    Changed from item to movie and from index to movieIndex;
    
    removed initial value line 18;
    
    Added handleRemoveClick inside FavoriteMovies and deleted seperate function

--- src/components/hooks/useReducer.jsx
+++ src/components/hooks/useReducer.jsx
@@ -1,11 +1,13 @@
+import React, { useState, useReducer } from 'react';
+
 function reducer(state, action) {
   switch (action.type) {
     case "add":
-      return [...state, action.item];
+      return [...state, { name: action.movie }];
     case "remove":
       return [
-        ...state.slice(0, action.index),
-        ...state.slice(action.index + 1)
+        ...state.slice(0, action.movieIndex),
+        ...state.slice(action.movieIndex + 1)
       ];
     default:
       throw new Error();
@@ -13,47 +15,41 @@ function reducer(state, action) {
 }
 
 function FavoriteMovies() {
-  const [movies, dispatch] = useReducer(reducer, [{ name: "Heat" }]);
+  const [movies, dispatch] = useReducer(reducer, []);
   const [newMovie, setNewMovie] = useState("");
 
   const handleAddClick = () => {
     if (newMovie === "") {
       return;
     }
-    dispatch({ type: "add", item: { name: newMovie } });
+    dispatch({ type: "add", movie: newMovie });
     setNewMovie("");
+  };
+
+  const handleRemoveClick = (index) => {
+    dispatch({ type: "remove", movieIndex: index });
   };
 
   return (
     <>
       <div className="movies">
-        {movies.map((movie, index) => {
-          return (
-            <Movie
-              movie={movie}
-              onRemove={() => dispatch({ type: "remove", index })}
-            />
-          );
-        })}
+        {movies.map((movie, index) => ( // movies.map - movies comes from FavoriteMovies() const [movies, dispatch] || index - .map comes with two statements always (name and index)
+          <div key={index} className="movie">
+            { <span>{movie.name}</span> /* Shows the value that was inputed corresponds to: return [...state, { name: action.movie }]; */}
+            <button onClick={() => handleRemoveClick(index)}>Remove</button>
+          </div>
+        ))}
       </div>
       <div className="add-movie">
         <input
           type="text"
           value={newMovie}
-          onChange={event => setNewMovie(event.target.value)}
+          onChange={(event) => setNewMovie(event.target.value)}
+          placeholder="Movie Name"
         />
         <button onClick={handleAddClick}>Add movie</button>
       </div>
     </>
-  );
-}
-
-function Movie({ movie, onRemove }) {
-  return (
-    <div className="movie">
-      <span>{movie.name}</span>
-      <button onClick={onRemove}>Remove</button>
-    </div>
   );
 }
 
